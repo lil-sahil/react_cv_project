0bb4706b42194ac372408fa720fe0de90b6b4e6c
lil-sahil <sahilnagpal93@yahoo.ca>
2022-06-04 14:09:00 -0400

    Added functionality to the createFormElements utility function to look at the current state and create jsx elements based on itemNames argument. Used the .include method. Example itemNames = [degree] but the state might inlude degree-1. Therefore degree-1.includes(degree) === true and the JSX element will be made.

--- src/utils/createFormElements.js
+++ src/utils/createFormElements.js
@@ -1,19 +1,26 @@
 export let createFormElements = (currentState, changeMethod, itemNames) => {
   let arr = [];
 
-  for (let itemName of itemNames) {
-    let labelName = itemName
-      .replace(/([A-Z])/g, " $1")
-      .replace(/^./, (x) => x.toUpperCase());
+  for (let stateName in currentState) {
+    for (let itemName of itemNames) {
+      if (stateName.includes(itemName)) {
+        let labelName = itemName
+          .replace(/([A-Z])/g, " $1")
+          .replace(/^./, (x) => x.toUpperCase());
 
-    let element = (
-      <div>
-        <label>{`${labelName}`}</label>
-        <input type="input" id={`${itemName}`} onChange={changeMethod}></input>
-      </div>
-    );
-    arr.push(element);
+        let element = (
+          <div>
+            <label>{`${labelName}`}</label>
+            <input
+              type="input"
+              id={`${itemName}`}
+              onChange={changeMethod}
+            ></input>
+          </div>
+        );
+        arr.push(element);
+      }
+    }
   }
-
   return arr;
 };
